d8b68f8bbcf112edb2bd85edeb7094d82807459b
york yao <yaoao12306@outlook.com>
2017-10-13 13:49:37 +0800

    refactor lint scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -3,6 +3,9 @@ const util = require('util')
 
 const execAsync = util.promisify(childProcess.exec)
 
+const tsFiles = `"src/**/*.ts" "spec/**/*.ts"`
+const jsFiles = `"*.config.js"`
+
 module.exports = {
   build: [
     'rimraf dist/',
@@ -10,9 +13,9 @@ module.exports = {
     'node dist/index.js demo/*.ts demo/*.less demo/*.scss --suppressError > demo/result.txt'
   ],
   lint: {
-    ts: `tslint "src/**/*.ts"`,
-    js: `standard "**/*.config.js"`,
-    export: `node dist/index.js src/*.ts`
+    ts: `tslint ${tsFiles}`,
+    js: `standard ${jsFiles}`,
+    export: `no-unused-export ${tsFiles}`
   },
   test: [
     'tsc -p spec',
@@ -26,8 +29,8 @@ module.exports = {
     }
   ],
   fix: {
-    ts: `tslint --fix "src/**/*.ts"`,
-    js: `standard --fix "**/*.config.js"`
+    ts: `tslint --fix ${tsFiles}`,
+    js: `standard --fix ${jsFiles}`
   },
   release: `clean-release`
 }
