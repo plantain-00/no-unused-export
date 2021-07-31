4a8154c5f275d23270c68804fd2e3f9b40c71608
York Yao <me@yorkyao.com>
2021-07-31 16:27:16 +0800

    chore: update dependencies

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -6,11 +6,11 @@ export default {
   build: [
     'rimraf dist/',
     'tsc -p src/',
-    'node dist/index.js demo/*.ts demo/*.tsx demo/*.less demo/*.scss src/*.ts --need-module tslib --ignore-module glob --strict --suppressError > demo/result.txt'
+    'node dist/index.js demo/*.ts demo/*.tsx demo/*.less demo/*.scss src/*.ts --need-module tslib --need-module postcss --ignore-module glob --strict --suppressError > demo/result.txt'
   ],
   lint: {
     ts: `eslint --ext .js,.ts,.tsx ${tsFiles}`,
-    export: `node dist/index.js ${tsFiles} --need-module tslib --strict`,
+    export: `node dist/index.js ${tsFiles} --need-module tslib --need-module postcss --strict`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'type-coverage -p src --strict'
   },
